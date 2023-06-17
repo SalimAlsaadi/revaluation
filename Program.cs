37a600122c56fbf90f38f21dd535a4e8da4ff7a7
SalimAlsaadi <al-saadis-s-s@hotmail.com>
2023-06-17 03:53:08 -0700

    good evaluation

---
+++ Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace revaluation
+{
+    internal class Program
+    {
+       
+        static void Main(string[] args)
+        {
+
+
+            SearchNumber srch = new SearchNumber();
+            
+            Console.WriteLine(srch.searchNumFromArry());
+
+            int[] arr = { 4, 7, 3, 5, 1, 10, 90, 8 };
+
+           
+            for(int i=0; i<arr.Length; i++)
+            {
+                for(int j=0; j < arr.Length-i-1; j++)
+                {
+                    if (arr[j] > arr[j+1] )
+                    {
+                        int temp;
+                        temp = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = temp;
+                        
+                      
+
+                    }
+                    
+                }
+                
+                   
+
+            }
+
+           for(int i=0; i<arr.Length; i++)
+            {
+               // Console.WriteLine(arr[i]);
+            }
+
+
+        }
+    }
+}
---
+++ SearchNumber.cs
@@ -0,0 +1,35 @@
+﻿
+using System;
+
+namespace revaluation
+{
+    class SearchNumber
+    {
+
+        int check;
+
+        public int searchNumFromArry()
+
+        {
+           
+            int[] searching = { 1, 3, 4, 5, 7, 8, 10, 90 };
+            
+            Console.WriteLine("enter number ");
+            string stringNum = Console.ReadLine();
+            int integerNum = int.Parse(stringNum);
+
+            check = Array.IndexOf(searching, integerNum);//this function to find index of item inside array. if user enter item that not found in array the retun will be -1.
+            if (check == 0)
+            {
+                //Console.WriteLine("print -1");
+            }
+
+            else
+            {
+
+            }
+
+            return check;
+        }
+    }
+}
